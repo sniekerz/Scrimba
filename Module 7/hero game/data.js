8f01c02828373be1d4eb4c8a93c22f2053839c99
sniekerz <85154061+sniekerz@users.noreply.github.com>
2022-08-11 16:50:08 +0200

    11-8

---
+++ Module 7/hero game/data.js
@@ -0,0 +1,16 @@
+const characterData = {
+  hero: {
+    name: "Wizard",
+    avatar: "images/wizard.jpg",
+    health: 60,
+    diceCount: 3,
+  },
+  monster: {
+    name: "Orc",
+    avatar: "images/orc.jpg",
+    health: 10,
+    diceCount: 1,
+  },
+};
+
+export default characterData;
---
+++ Module 7/learning new things/index.js
@@ -0,0 +1,200 @@
+//Aim: create an array of length ten. The elements should be undefined.
+
+// const endOfLevelBosses = [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined]
+
+// const endOfLevelBosses = []
+
+// for (let i=0; i<10; i++){
+//     endOfLevelBosses.push(undefined)
+// }
+
+// const endOfLevelBosses = new Array(10);
+
+// console.log(endOfLevelBosses);
+
+/*
+Challenge
+1. Create a new array with a length of 1000.
+2. Log out the new array.
+3. Don't worry about the emoji yet!
+*/
+
+// const goldCoins = new Array(10)
+//   .fill("🪙")
+//   .map(function (coins) {
+//     return `<div class="box">${coins}</div>`;
+//   })
+//   .join("");
+// console.log(goldCoins.length);
+
+// /*
+// Challenge
+// 1. Create a new array with a length of 1000 and save it to a
+//    const called poisonMushrooms
+// 2. Convert each element of the array to a mushroom emoji 🍄
+// 3. Log out the new array
+// */
+
+// const poisonMushrooms = new Array(10)
+//   .fill("🍄")
+//   .map(function (mushroom) {
+//     return `<div class="box">${mushroom}</div>`;
+//   })
+//   .join("");
+// console.log(poisonMushrooms.length);
+
+// document.getElementById("mushrooms").innerHTML = poisonMushrooms + goldCoins;
+
+// CONSTRUCTOR CLASS
+// SIMPLE CONSTRUCTOR
+// function Animal() {
+//   this.species = "tiger";
+//   this.weightKg = 56;
+//   this.age = 2;
+// }
+
+// const animal1 = new Animal();
+
+// console.log(animal1);
+
+// const animalForRelease1 = {
+//   name: "Tilly",
+//   species: "tiger",
+//   weightKg: 56,
+//   age: 2,
+//   dateOfRelease: "03-02-2022",
+// };
+// const animalForRelease2 = {
+//   name: "Nelly",
+//   species: "elephant",
+//   weightKg: 320,
+//   age: 16,
+//   dateOfRelease: "03-02-2022",
+// };
+
+// function Animal(data) {
+//   this.name = data.name;
+//   this.species = data.species;
+//   this.weightKg = data.weightKg;
+//   this.age = data.age;
+//   this.dateOfRelease = data.dateOfRelease;
+// }
+
+// const tillyTheTiger = new Animal(animalForRelease1);
+// const nellyTheElephant = new Animal(animalForRelease2);
+
+// console.log(tillyTheTiger, nellyTheElephant);
+
+// const hotel1 = {
+//   name: "Safari View",
+//   rooms: 30,
+//   stars: "⭐⭐⭐⭐⭐",
+//   costPerNightAdult: 240,
+//   costPerNightChild: 180,
+// };
+
+// const hotel2 = {
+//   name: "Leopard Mansion",
+//   rooms: 96,
+//   stars: "⭐⭐⭐",
+//   costPerNightAdult: 120,
+//   costPerNightChild: 180,
+// };
+// // /*
+// // Challenge:
+// // 1. Create a constructor function called NationalParkHotels.
+// // 2. Have it take in "data" as a parameter
+// // 3. Assign the data to "this"
+// // 4. Log out the result of creating an instance of
+// //    NationalParkHotels for each hotel.
+// // */
+
+// function NationalParkHotels(data) {
+//   this.name = data.name;
+//   this.rooms = data.rooms;
+//   this.stars = data.stars;
+//   this.costPerNightAdult = data.costPerNightAdult;
+//   this.costPerNightChild = data.costPerNightChild;
+//   this.summariseHotel = function () {
+//     const totalPrice = this.costPerNightAdult * 2 + this.costPerNightChild * 2;
+
+//     console.log(`A one night stay at the ${this.name}
+//     for two adults and two children costs a total of
+//     ${totalPrice} dollars`);
+//   };
+// }
+
+// const leopardMansion = new NationalParkHotels(hotel2);
+// const safariView = new NationalParkHotels(hotel1);
+// safariView.summariseHotel();
+// leopardMansion.summariseHotel();
+
+/*
+Challenge:
+1. Create a method on the constructor function called
+   "summariseHotel".
+2. Have it log out the following sentence 'A one night stay 
+   at the <HOTEL NAME> for two adults and two children costs a 
+   total of <PRICE>'
+3. Your method will need the logic to calculate the price.
+*/
+
+// const studentDetails = {
+//   firstName: "janaka",
+//   lastName: "siriwardena",
+//   age: 28,
+//   country: "sri lanka",
+//   email: "j.siri@totalinternet.com",
+//   discordUsername: "JS1",
+// };
+
+// function Student(data) {
+//   // this.firstName = data.firstName;
+//   // this.lastName = data.lastName;
+//   // this.age = data.age;
+//   // this.country = data.country;
+//   // this.email = data.email;
+//   // this.discordUsername = data.discordUsername;
+//   Object.assign(this, data);
+//   this.summariseStudent = function () {
+//     console.log(`${this.firstName} ${this.lastName} is ${this.age} years old
+//       and comes from ${this.country}. Their email is ${this.email} and you can find them on discord as ${this.discordUsername}`);
+//   };
+// }
+
+// const newStudent = new Student(studentDetails);
+// newStudent.summariseStudent();
+
+// const sandraKayeProfileData = {
+// 	name: 'Sandra Kaye',
+// 	portfolio: 'www.sandrasportfolio.com',
+// 	currentJob: 'Google',
+// 	currentSalary: '400k'
+// }
+
+// /*
+// Challenge
+// 1. Use Object.assign to cut 4 lines of code from the constructor function.
+// */
+// const sandraKayeProfileData = {
+//   name: "Sandra Kaye",
+//   portfolio: "www.sandrasportfolio.com",
+//   currentJob: "Google",
+//   currentSalary: "400k",
+// };
+
+// function DevProfile(data) {
+//   // this.name = data.name;
+//   // this.portfolio = data.portfolio;
+//   // this.currentJob = data.currentJob;
+//   // this.currentSalary = data.currentSalary;
+//   Object.assign(this, data);
+//   this.summariseDev = function () {
+//     console.log(
+//       `${this.name}'s porfolio is at ${this.portfolio} and they work at ${this.currentJob}. Their current salary is ${this.currentSalary}.`
+//     );
+//   };
+// }
+
+// const sandraKaye = new DevProfile(sandraKayeProfileData);
+// sandraKaye.summariseDev();
