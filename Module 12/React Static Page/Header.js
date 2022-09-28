ba3f76f53590bdfec18102bad11fdc59b8da22a2
sniekerz <85154061+sniekerz@users.noreply.github.com>
2022-09-28 16:58:11 +0200

    28-9

---
+++ Module 12/React Static Page/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header>
+      <nav className="nav">
+        <img src="react-logo.png" className="nav-image"></img>
+        <ul className="nav-items">
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
--- Module 12/React Static Page/index.js
+++ Module 12/React Static Page/index.js
@@ -1,104 +1,31 @@
-/* Surprise! You probably thought you could just forget
-the line of code you just learned! Nope, not on my watch!
+function MainContent() {
+  return (
+    <div>
+      <h1>Reasons for me to learn React!</h1>
+      <ol>
+        <li>Its awesome</li>
+        <li>I want to program</li>
+        <li>I need it to be my career</li>
+      </ol>
+    </div>
+  );
+}
 
-Try to write that 1-liner of React code again! This time,
-see if you can figure out how to render an <ul> with 2+ <li>s inside*/
+function Footer() {
+  return (
+    <footer className="footer">
+      <small>© 2022 Brouwer development. All rights reserved.</small>
+    </footer>
+  );
+}
 
-// ReactDOM.render(
-//   <ul>
-//     <li>First</li>
-//     <li>Second</li>
-//   </ul>,
-//   document.getElementById("root")
-// );
+function Page() {
+  return (
+    <div>
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
 
-// const page = (
-//   <div>
-//     <h1 className="header">This is JSX</h1>
-//     <p>This is a paragraph</p>
-//   </div>
-// );
-
-// console.log(page);
-
-// ReactDOM.render(page, document.getElementById("root"));
-
-/* 
-Challenge: 
-
-Create a navbar in JSX:
-    - Use the semantic `nav` element as the parent wrapper
-    - Have an h1 element with the brand name of your "website"
-    - Insert an unordered list for the other nav elements
-        - Inside the `ul`, have three `li`s for "Pricing",
-        "About", and "Contact"
-    - Don't worry about styling yet - it'll just be plain-looking HTML for now
-*/
-
-// const navBar = (
-//   <nav>
-//     <h1>WONDER</h1>
-//     <ul>
-//       <li>Pricing</li>
-//       <li>About</li>
-//       <li>Contact</li>
-//     </ul>
-//   </nav>
-// );
-
-// ReactDOM.render(navBar, document.getElementById("root"));
-// import React from "react";
-// import ReactDOM from "react-dom";
-
-/**
-Challenge: fix our code!
-
-Don't forget, you're not using CDNs anymore, so there's no
-global "ReactDOM" variable to use anymore.
- */
-
-// const page = (
-//   <div>
-//     <h1>My awesome website in React</h1>
-//     <h3>Reasons I love React</h3>
-//     <ol>
-//       <li>It's composable</li>
-//       <li>It's declarative</li>
-//       <li>It's a hireable skill</li>
-//       <li>It's actively maintained by skilled people</li>
-//     </ol>
-//   </div>
-// );
-
-// ReactDOM.render(page, document.getElementById("root"));
-
-/*
-Challenge: Starting from scratch, build and render the 
-HTML for our section project. Check the Google slide for 
-what you're trying to build.
-
-We'll be adding styling to it later.
-
-Hints:
-* The React logo is a file in the project tree, so you can
-  access it by using `src="./react-logo.png" in your image
-  element
-* You can also set the `width` attribute of the image element
-  just like in HTML. In the slide, I have it set to 40px
- */
-
-const page = (
-  <div>
-    <img src="react-logo.png"></img>
-    <h1>Fun facts about React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has wel over 100K stars on Github</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-  </div>
-);
-
-ReactDOM.render(page, document.getElementById("root"));
+ReactDOM.render(<Page />, document.getElementById("root"));
