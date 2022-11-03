be923509686884456a70c5b3bb4b5f2590fb7659
sniekerz <85154061+sniekerz@users.noreply.github.com>
2022-11-03 16:01:56 +0100

    3-11 4

--- Module 5/Playground/index.js
+++ Module 5/Playground/index.js
@@ -1,4 +1,58 @@
-document.addEventListener("click", function (e) {
-  document.getElementById(e.target.id).parentElement.classList.add("read");
-  document.getElementById(e.target.id).parentElement.classList.remove("unread");
+const ages = [1, 5, 9, 23, 56, 10, 47, 70, 10, 19, 23, 18];
+
+const adults = ages.filter(function (age) {
+  return age >= 18;
 });
+
+// console.log(adults.length);
+
+const children = ages.filter(function (age) {
+  return age < 18;
+});
+
+// console.log(children.length, children);
+
+const series = [
+  {
+    name: "The Wire",
+    location: "Baltimore",
+    lengthInHours: 60,
+    genres: ["action", "thriller", "detective", "suspense"],
+  },
+  {
+    name: "Game of Thromes",
+    location: "Westeros and Essos",
+    lengthInHours: 70.25,
+    genres: ["fantasy", "action", "tragedy"],
+  },
+  {
+    name: "Friends",
+    location: "New York",
+    lengthInHours: 85,
+    genres: ["comedy", "romance", "drama"],
+  },
+  {
+    name: "The Walking Dead",
+    location: "Atlanta",
+    lengthInHours: 131,
+    genres: ["zombie", "apocalypse", "thriller", "suspense"],
+  },
+  {
+    name: "The Big Bang Theory",
+    location: "Pasadena",
+    lengthInHours: 139.66,
+    genres: ["comedy", "nerd", "romance"],
+  },
+];
+
+const newYorkSeries = series.filter(function (show) {
+  return show.location === "New York";
+});
+
+// console.log(newYorkSeries);
+
+const thrillers = series.filter(function (show) {
+  return show.genres.includes("thriller");
+});
+
+console.log(thrillers);
